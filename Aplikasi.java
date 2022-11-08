88796913f6b3958103d0ce27db076bd1219e6420
toosakarin1412 <toosakarin1412@gmail.com>
2022-11-08 16:05:49 +0700

    update some file

---
+++ Aplikasi.java
@@ -0,0 +1,9 @@
+import app.*;
+import database.*;
+import gui.*;
+
+public class Aplikasi{
+    public static void main(String[] args){
+        // Do something
+    };
+}
---
+++ Testing.java
@@ -0,0 +1,9 @@
+import app.*;
+import database.*;
+import gui.*;
+
+public class Testing{
+    public static void main(String[] args){
+        // Do something
+    };
+}
