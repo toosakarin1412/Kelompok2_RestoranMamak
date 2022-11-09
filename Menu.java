d2b8e7f1996608ab34e0355d69d0583bd8d0d354
raishabirah <raihanshabirah20@gmail.com>
2022-11-09 23:16:33 +0700

    added class menu

---
+++ Menu.java
@@ -0,0 +1,29 @@
+public abstract class Menu {
+    private String name;
+    private int price;
+    private String picture;
+
+    public void setName(){
+
+    }
+
+    public void setPrice(){
+
+    }
+
+    public void setPicture(){
+
+    }
+
+    public String getName(){
+
+    }
+
+    public int getPrice(){
+
+    }
+
+    public String getPicture(){
+        
+    }
+}
