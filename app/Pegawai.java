ac9de51abfced781e9759e4eec4e110c4d7607b9
furqanramadhan <furqan21@mhs.unsyiah.ac.id>
2022-11-10 19:35:34 +0700

    update Pegawai.java

---
+++ app/Pegawai.java
@@ -0,0 +1,22 @@
+package app;
+
+public abstract class Pegawai {
+    private String name;
+    private String phone;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public void setPhone() {
+        this.phone = phone;
+    }
+}
