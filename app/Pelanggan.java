b4a75f81aec0b9de181b014eadb212575eb09bd9
Tasya Nadila <tasyanadila28012004@gmail.com>
2022-11-10 08:52:04 +0700

    add class pelanggan

---
+++ app/Pelanggan.java
@@ -0,0 +1,38 @@
+package app;
+public class Pelanggan{
+    private String name;
+    private int table_no;
+    private boolean takeaway;
+
+    public Pelanggan(){
+
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public void setTableNo(){
+        this.table_no = table_no;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getTableName(){
+        return table_no;
+    }
+
+    public void addOrder(Menu Pesan){
+
+    }
+
+    public Menu getOrder(){
+
+    }
+
+    public void setTakeaway(){
+        this.takeaway = takeaway;
+    }
+}
