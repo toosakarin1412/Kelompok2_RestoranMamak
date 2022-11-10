744854195ab68318da614d7f4bd0ee4f20de36a9
furqanramadhan <furqan21@mhs.unsyiah.ac.id>
2022-11-10 20:35:09 +0700

    update

--- app/Pelanggan.java
+++ app/Pelanggan.java
@@ -1,38 +1,39 @@
 package app;
-public class Pelanggan{
+
+public class Pelanggan {
     private String name;
     private int table_no;
     private boolean takeaway;
 
-    public Pelanggan(){
+    public Pelanggan() {
 
     }
 
-    public void setName(){
+    public void setName() {
         this.name = name;
     }
 
-    public void setTableNo(){
+    public void setTableNo() {
         this.table_no = table_no;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getTableName(){
+    public int getTableName() {
         return table_no;
     }
 
-    public void addOrder(Menu Pesan){
+    public void addOrder(Menu Pesan) {
 
     }
 
-    public Menu getOrder(){
+    public Menu getOrder() {
 
     }
 
-    public void setTakeaway(){
+    public void setTakeaway() {
         this.takeaway = takeaway;
     }
 }
