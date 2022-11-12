e3de81158812d80af22d140408dcb0bb09100cdc
furqanramadhan <furqan21@mhs.unsyiah.ac.id>
2022-11-12 08:09:00 +0700

    Update Pegawai.java

--- app/Pegawai.java
+++ app/Pegawai.java
@@ -1,20 +1,44 @@
 package app;
 
+/**
+ * Menyatakan class bernama Pegawai untuk
+ * mendapatkan nama, dan nomor hp pegawai.
+ * 
+ * @author Kelompok2_RestoranMamak
+ * @version 1.0
+ */
 public abstract class Pegawai {
     private String name;
     private String phone;
 
+    /**
+     * @return Method accessor untuk mengembalikan
+     *         nama kedalam pegawai.
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * 
+     * @return Method accessor untuk mengembalikan
+     *         phone kedalam pegawai.
+     */
 
     public String getPhone() {
         return this.phone;
     }
 
+    /**
+     * @param method mutator untuk setter nama.
+     */
     public void setName() {
         this.name = name;
     }
+
+    /**
+     * @param method mutator untuk setter phone.
+     */
 
     public void setPhone() {
         this.phone = phone;
