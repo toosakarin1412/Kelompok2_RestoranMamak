5112e2c86fbb0e39b2ab1eb251f1af7b20f79cc2
raishabirah <raihanshabirah20@gmail.com>
2022-11-14 10:23:52 +0700

    add javadoc

--- app/Menu.java
+++ app/Menu.java
@@ -1,29 +1,58 @@
 package app;
+
+/**
+ * Abstract class Menu
+ * @author Kelompok2_RestoranMamak
+ */
 public abstract class Menu {
     private String name;
     private int price;
     private String picture;
 
+    /**
+     * Mengatur nama dari menu
+     * @param nama nama menu
+     */
     public void setName(){
-
+        this.name = name;
     }
 
+    /**
+     * Mengatur harga dari menu
+     * @param price harga menu
+     */
     public void setPrice(){
-
+        this.price = price;
     }
 
+    /**
+     * Mengatur gambar dari menu
+     * @param picture gambar menu
+     */
     public void setPicture(){
-
+        this.picture = picture;
     }
 
+    /**
+     * Mengembalikan nama dari menu
+     * @return name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Mengembalikan harga dari menu
+     * @return price
+     */
     public int getPrice(){
         return this.price;
     }
 
+    /**
+     * Mengembalikan gambar dari menu
+     * @return picture
+     */
     public String getPicture(){
         return this.picture;
     }
